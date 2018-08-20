afcb80967edd2021e74d95049868b61853c1e93f
Vladislav Shamanov <vladsham2470@yandex.ru>
2018-08-20 15:40:44 +0700

    Ангулар проба_1
    Signed-off-by: Vladislav Shamanov <vladsham2470@yandex.ru>

---
+++ about_me/push/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogoComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
